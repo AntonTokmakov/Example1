d87f4871f23668dd1a984139033d32a4675d89ad
Anton <antontokmakov757@gmail.com>
2023-07-08 14:23:06 +0700

    Thirst example complited

---
+++ ДЗ3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите чисто, что бы проверить его на счетность");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+} else
+{
+    Console.WriteLine("Нет");
+}
