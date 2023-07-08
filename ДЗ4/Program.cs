267f8c2215eaaf0cea53189d6a3e39f062854ab4
Anton <antontokmakov757@gmail.com>
2023-07-08 14:43:08 +0700

    Fourth example complited

---
+++ ДЗ4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вывод");
+for (int i = 1; i < a + 1; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
